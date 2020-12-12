8132ebdb99f3e7bc9a6121e89ab33841978a2ffc
ko-suke <nako2222ns600mab-2@outlook.jp>
2020-12-12 17:41:18 +0900

    123

--- js/common.js
+++ js/common.js
@@ -3,8 +3,22 @@ const userName = document.getElementById('user-name');
 const myRank = document.getElementById('my-rank');
 const myScore = document.getElementById('my-score');
 
+var userAgent = window.navigator.userAgent.toLowerCase();
+    
+var cosoleText = document.getElementById('console_log');
+if(userAgent.indexOf('msie') != -1 || userAgent.indexOf('trident') != -1) {
+    //IE向けの記述
+    cosoleText.textContent = "IEは、使えません";
+    console.log('IEは、使えません');
+
+}else if(userAgent.indexOf('chrome') != -1) {
+    //Google Chrome向けの記述  
+    cosoleText.textContent = "Chrome";
+    console.log('chrome');
+};
+
 function setUserName() {
-    let myName = prompt('あなたの名前を入力してください。');
+    let myName = prompt('あなたの名前を入力してください(ニックネームも可）');
     localStorage.setItem('user-name', myName);
     userName.textContent = myName;
 
@@ -25,7 +39,7 @@ if (!localStorage.getItem('user-name')) {
 const btn = document.querySelector('.btn-menu');
 const nav = document.querySelector('nav');
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', function() {
   nav.classList.toggle('open-menu')
 });
 
